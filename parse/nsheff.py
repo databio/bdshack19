922d269954d5e7a5402b1625da05e91122929e76
nsheff <nsheff@users.noreply.github.com>
2019-04-04 15:48:23 -0400

    reading Matrix Market files

---
+++ parse/nsheff.py
@@ -0,0 +1,8 @@
+import scipy
+import scipy.io
+
+scipy.io.mmread("/project/shefflab/bdshack")
+
+m = scipy.io.mmread("/project/shefflab/bdshack19/sciCAR/rawdata/GSE117089_RAW/GSM3271045_ATAC_mouse_kidney_peak_count.txt.gz")
+
+
