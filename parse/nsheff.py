da566fe2270602e63d476229000c61a7e6dc3626
nsheff <nsheff@users.noreply.github.com>
2019-04-04 16:04:57 -0400

    add some parsing

--- parse/nsheff.py
+++ parse/nsheff.py
@@ -3,6 +3,18 @@ import scipy.io
 
 scipy.io.mmread("/project/shefflab/bdshack")
 
-m = scipy.io.mmread("/project/shefflab/bdshack19/sciCAR/rawdata/GSE117089_RAW/GSM3271045_ATAC_mouse_kidney_peak_count.txt.gz")
+a = scipy.io.mmread("/project/shefflab/bdshack19/sciCAR/rawdata/GSE117089_RAW/GSM3271045_ATAC_mouse_kidney_peak_count.txt.gz")
+
+
+g = scipy.io.mmread("/project/shefflab/bdshack19/sciCAR/rawdata/GSE117089_RAW/GSM3271044_RNA_mouse_kidney_gene_count.txt.gz")
+
+
+
+# get dimensions with: 
+a.shape
+g.shape
+
+import pandas
+pks = pandas.read_csv("/project/shefflab/bdshack19/sciCAR/rawdata/GSE117089_RAW//GSM3271045_ATAC_mouse_kidney_peak.txt.gz")
 
 
