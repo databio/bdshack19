b902c43fcdcf4fc6feaf9eccb274d12d1a01bad7
nsheff <nsheff@users.noreply.github.com>
2019-04-04 16:44:13 -0400

    add linking code; some object work

--- parse/nsheff.py
+++ parse/nsheff.py
@@ -2,9 +2,6 @@ import pandas
 import scipy
 import scipy.io
 
-scipy.io.mmread("/project/shefflab/bdshack")
-
-
 # Data matrices
 # peaks by cells
 a = scipy.io.mmread("/project/shefflab/bdshack19/sciCAR/rawdata/GSE117089_RAW/GSM3271045_ATAC_mouse_kidney_peak_count.txt.gz")
@@ -36,10 +33,21 @@ g.shape
 tm_genes = TripleMatrix(g, gc, genes)
 tm_atac = TripleMatrix(a, ac, pks)
 
+# linking two modalities:
+
+pd.merge(ac, gc, how="outer", on="sample")
+pd.merge(ac, gc, how="inner", on="sample").iloc[1:5,:]
+
+
 
 
 class TripleMatrix(object):
+	"""
+	Representation of a single modality
 
+	It's made of 3 matrices: A data matrix, a column annotation
+	matrix, and a row annotation matrix.
+	"""
 	def __init__(self, data, cell_anno, feature_anno):
 		self.data = data
 		self.cell_anno = cell_anno
@@ -48,8 +56,13 @@ class TripleMatrix(object):
 
 
 class SSMultiHolder(object):
-
-	def __init__():
+	"""
+	Holds multiple modalities
+	"""
+	def __init__(self, modality_list):
+		"""
+		:param modality_list list[TripleMatrix]: A list of data objects containing information about each modality.
+		"""
 		print("Hello!")
 
 
